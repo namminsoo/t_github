46b0e08c6fa114daf7914504460b0a4550c79fb0
NamSoo <ukun3147@naver.com>
2016-12-26 14:10:26 +0900

    Update sample.js on Github

--- sample.js
+++ sample.js
@@ -3,4 +3,4 @@ console.log('Tell your world');
 console.log('Tell his world');
 console.log('Tell her world');
 console.log('Tell our world');
-consoel.log('Tell my world');
+console.log('Tell my world');
